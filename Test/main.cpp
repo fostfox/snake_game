5db3f4910b33e6f4e4a4298b242fce5c8ea90a16
ArnMips <arzhevitin.bk@gmail.com>
2017-04-07 22:31:51 +0300

    update test
    	modified:   Test/main.cpp

--- Test/main.cpp
+++ Test/main.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout << "Hello World and a Bogdan" << endl;
+    for (int i = 0 ; i < 5; ++i) {
+        count << i << endl;
+    }
     return 0;
 }
